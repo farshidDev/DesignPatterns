8bc9219cd9a7ad277e7e674c6777276ec5a30d7d
farshid ahmadi <ahmadi.farshid108@gmail.com>
2021-12-03 04:37:55 +0330

    singleton pattern

---
+++ SingletonPattern/Classes/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern.Classes
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        public string Data = "some data";
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using SingletonPattern.Classes;
+using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance = Singleton.GetInstance();
+            Console.WriteLine(instance.Data);
+
+            Singleton secondInstance = Singleton.GetInstance();
+            secondInstance.Data = "new data";
+
+            Console.WriteLine(secondInstance.Data);
+            Console.WriteLine(instance.Data);
+        }
+    }
+}
